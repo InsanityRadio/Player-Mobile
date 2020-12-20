55d73e3290c441bf862774e64ce8b72a8c3f2c12
Jamie Woods <jamie@index.hm>
2020-12-20 16:00:04 +0000

    make dev mode work

--- android/app/src/main/java/com/insanityradio/insanityradio/MainApplication.java
+++ android/app/src/main/java/com/insanityradio/insanityradio/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.BuildConfig;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.insanityradio.insanityradio.generated.BasePackageList;
